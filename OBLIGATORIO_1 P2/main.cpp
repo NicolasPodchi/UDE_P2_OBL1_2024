e186684ebd1f9fbbe9022726c1212a3e6ad02c48
Nuñez, Federico <fnunez@gicla.com>
2024-09-13 17:54:28 -0300

    typedefs

--- OBLIGATORIO_1 P2/main.cpp
+++ OBLIGATORIO_1 P2/main.cpp
@@ -9,3 +9,48 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+
+typedef struct
+{
+    long int cedula;
+    str nombre;
+    str apellido;
+    date fechaNacimiento;
+    time horaIngreso;
+    tipoBailarin discriminante;
+
+    union
+    {
+        int cantidadPremios;
+        str estiloTango;
+        paises nacionalidad;
+    } datos;
+
+} bailarin;
+
+typedef enum
+{
+    INFANTIL,
+    JUVENIL,
+    ADULTO
+
+} tipoBailarin;
+
+typedef enum
+{
+    URUGUAY,
+    BRASIL,
+    ARGENTINA,
+    PARAGUAY,
+    OTRO
+
+} paises;
+
+const int TAM = 50;
+typedef struct
+{
+    bailarin arre[TAM];
+    int tope;
+} muestra;
+
+

4906c2bb2d6f876cfee128e1a491f402598a053c
Faustino Santos <faus.santos@hotmail.com>
2024-09-13 19:09:50 -0300

    Tipodatos adentro y muestra medio adentro

---
+++ OBLIGATORIO_1 P2/1.Auxiliares/paises.h
@@ -0,0 +1,14 @@
+#ifndef PAISES_H_INCLUDED
+#define PAISES_H_INCLUDED
+typedef enum
+{
+    URUGUAY,
+    BRASIL,
+    ARGENTINA,
+    PARAGUAY,
+    OTRO
+
+} paises;
+
+
+#endif // PAISES_H_INCLUDED
---
+++ OBLIGATORIO_1 P2/1.Auxiliares/tipoBailarin.h
@@ -0,0 +1,12 @@
+#ifndef TIPOBAILARIN_H_INCLUDED
+#define TIPOBAILARIN_H_INCLUDED
+typedef enum
+{
+    INFANTIL,
+    JUVENIL,
+    ADULTO
+
+} tipoBailarin;
+
+
+#endif // TIPOBAILARIN_H_INCLUDED
